0c2da215fdeb49db57a93a7202f510a908f105e3
dkolevv <dkolev2323@gmail.com>
2025-01-22 00:22:47 +0200

    Add cart page, scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,15 @@ function handleSubmit(event) {
   submitButton.textContent = "Sent";
   submitButton.style.backgroundColor = "#28a745";
 }
+function addToCart(productId, productName, productPrice) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push({ id: productId, name: productName, price: productPrice });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(productName + " has been added to your cart!");
+}
+function removeFromCart(productId) {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart = cart.filter((product) => product.id !== productId);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  location.reload();
+}
